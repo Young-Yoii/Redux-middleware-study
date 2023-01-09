b0309475bcb439793da6b35535602c65be679f4f
Young-yoii <fhqht4545@gmail.com>
2023-01-09 21:19:07 +0900

    2. 미들웨어 만들어보기

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
+import myLogger from './middlewares/myLogger';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(myLogger, logger)); // 여러개의 미들웨어를 적용 할 수 있습니다.);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
