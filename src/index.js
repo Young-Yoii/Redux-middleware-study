a9ee884e2ee90978da975e48e0e041b46aa5d1be
Young-yoii <fhqht4545@gmail.com>
2023-01-10 14:35:04 +0900

    redux-thunk의 기본 예시

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
 import logger from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(logger)); // 여러개의 미들웨어를 적용 할 수 있습니다.);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk, logger)); // 여러개의 미들웨어를 적용 할 수 있습니다.);
+//logger을 사용할 경우 logger가 가장 마지막에 와야함
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
